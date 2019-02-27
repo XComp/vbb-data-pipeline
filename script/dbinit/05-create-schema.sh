7a72c480d6fd169f1e0d27abd8c53957afa7f4a7
Matthias Pohl <XComp@gmx.de>
2019-02-27 15:29:34 +0100

    Extended schema and removed NOT NULL labels if not needed. Additionally, frequencies table was added.

--- script/dbinit/05-create-schema.sh
+++ script/dbinit/05-create-schema.sh
@@ -9,13 +9,13 @@ PGPASSWORD="$GTFS_DB_PASSWORD" psql -v ON_ERROR_STOP=1 -d "$GTFS_DB" -U "$GTFS_D
     CREATE TABLE gtfs.provider (
         provider_id             VARCHAR(8) PRIMARY KEY NOT NULL,
         created                 TIMESTAMP NOT NULL,
-        feed_publisher_name     TEXT NOT NULL,
-        feed_publisher_url      TEXT NOT NULL,
-        feed_lang               TEXT NOT NULL
+        feed_publisher_name     TEXT,
+        feed_publisher_url      TEXT,
+        feed_lang               TEXT
     );
 
     CREATE TABLE gtfs.run (
-        run_id                  INT PRIMARY KEY NOT NULL,
+        run_id                  SERIAL PRIMARY KEY NOT NULL,
         run_date                VARCHAR(10) NOT NULL,
         provider_id             VARCHAR(8) NOT NULL REFERENCES gtfs.provider(provider_id)
     );
@@ -23,25 +23,27 @@ PGPASSWORD="$GTFS_DB_PASSWORD" psql -v ON_ERROR_STOP=1 -d "$GTFS_DB" -U "$GTFS_D
     CREATE TABLE gtfs.agency (
         run_id                  INT NOT NULL REFERENCES gtfs.run(run_id),
         agency_id               TEXT PRIMARY KEY NOT NULL,
-        agency_name             TEXT NOT NULL,
-        agency_url              TEXT NOT NULL,
-        agency_timezone         TEXT NOT NULL,
+        agency_name             TEXT NULL,
+        agency_url              TEXT NULL,
+        agency_timezone         TEXT NULL,
         agency_lang             TEXT NULL,
-        agency_phone            TEXT NULL
+        agency_phone            TEXT NULL,
+        agency_fare_url         TEXT NULL
     );
 
     CREATE TABLE gtfs.stops (
         run_id                  INT NOT NULL REFERENCES gtfs.run(run_id),
         stop_id                 TEXT PRIMARY KEY,
         stop_code               TEXT NULL,
-        stop_name               TEXT NOT NULL,
+        stop_name               TEXT NULL,
         stop_desc               TEXT NULL,
         stop_lat                DOUBLE PRECISION NOT NULL,
         stop_lon                DOUBLE PRECISION NOT NULL,
         zone_id                 TEXT NULL,
         stop_url                TEXT NULL,
         location_type           BOOLEAN NULL,
-        parent_station          TEXT NULL
+        parent_station          TEXT NULL,
+        stop_timezone           TEXT NULL
     );
 
     CREATE TABLE gtfs.routes (
@@ -74,16 +76,16 @@ PGPASSWORD="$GTFS_DB_PASSWORD" psql -v ON_ERROR_STOP=1 -d "$GTFS_DB" -U "$GTFS_D
     CREATE TABLE gtfs.calendar_dates (
         run_id                  INT NOT NULL REFERENCES gtfs.run(run_id),
         service_id              TEXT NOT NULL,
-        date                    NUMERIC(8) NOT NULL,
-        exception_type          INT NOT NULL
+        date                    NUMERIC(8) NULL,
+        exception_type          INT NULL
     );
 
     CREATE TABLE gtfs.shapes (
         run_id                  INT NOT NULL REFERENCES gtfs.run(run_id),
         shape_id                TEXT,
-        shape_pt_lat            DOUBLE PRECISION NOT NULL,
-        shape_pt_lon            DOUBLE PRECISION NOT NULL,
-        shape_pt_sequence       INT NOT NULL,
+        shape_pt_lat            DOUBLE PRECISION NULL,
+        shape_pt_lon            DOUBLE PRECISION NULL,
+        shape_pt_sequence       INT NULL,
         shape_dist_traveled     TEXT NULL
     );
 
@@ -101,10 +103,10 @@ PGPASSWORD="$GTFS_DB_PASSWORD" psql -v ON_ERROR_STOP=1 -d "$GTFS_DB" -U "$GTFS_D
     CREATE TABLE gtfs.stop_times (
         run_id                  INT NOT NULL REFERENCES gtfs.run(run_id),
         trip_id                 TEXT NOT NULL,
-        arrival_time            INTERVAL NOT NULL,
-        departure_time          INTERVAL NOT NULL,
+        arrival_time            INTERVAL NULL,
+        departure_time          INTERVAL NULL,
         stop_id                 TEXT NOT NULL,
-        stop_sequence           INT NOT NULL,
+        stop_sequence           INT NULL,
         stop_headsign           TEXT NULL,
         pickup_type             INT NULL CHECK(pickup_type >= 0 and pickup_type <=3),
         drop_off_type           INT NULL CHECK(drop_off_type >= 0 and drop_off_type <=3),
@@ -113,9 +115,18 @@ PGPASSWORD="$GTFS_DB_PASSWORD" psql -v ON_ERROR_STOP=1 -d "$GTFS_DB" -U "$GTFS_D
 
     CREATE TABLE gtfs.transfers (
         run_id                  INT NOT NULL REFERENCES gtfs.run(run_id),
-        from_stop_id            TEXT NOT NULL,
-        to_stop_id              TEXT NOT NULL,
-        transfer_type           INT NOT NULL,
+        from_stop_id            TEXT NULL,
+        to_stop_id              TEXT NULL,
+        transfer_type           INT NULL,
         min_transfer_time       INT
+    );
+
+    CREATE TABLE gtfs.frequencies (
+        run_id                  INT NOT NULL REFERENCES gtfs.run(run_id),
+        trip_id                 TEXT NOT NULL,
+        start_time              TEXT NULL,
+        end_time                TEXT NULL,
+        headway_secs            TEXT NULL,
+        exact_times             TEXT NULL
     );
 EOSQL
